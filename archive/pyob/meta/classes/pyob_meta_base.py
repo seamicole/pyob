fbb30ae69c99b9c0f0556d7273a7ba2827e9e27e
Sean Michael O'Leary <seamicole@gmail.com>
2022-11-09 23:05:19 +0800

    Create archive

--- archive/pyob/meta/classes/pyob_meta_base.py
+++ archive/pyob/meta/classes/pyob_meta_base.py
@@ -1,34 +1,52 @@
 # ┌─────────────────────────────────────────────────────────────────────────────────────
-# │ PYOB META BASE
+# │ GENERAL IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from pydantic import BaseModel
+from typing import Type
+
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ PYOB META MODEL
 # └─────────────────────────────────────────────────────────────────────────────────────
 
 
-class PyObMetaBase:
-    """An attribute blueprint for all PyObClass.PyObMeta definitions"""
+class PyObMetaModel(BaseModel):
+    """An Pydantic model for all PyObClass.PyObMeta definitions"""
 
     # ┌─────────────────────────────────────────────────────────────────────────────────
     # │ RELATIVES
     # └─────────────────────────────────────────────────────────────────────────────────
 
     # Initialize list of parent classes to None
-    Parents = None
+    Parents: list[Type["PyOb"]] = None
 
     # Initialize list of child classes to None
-    Children = None
+    Children: list[Type["PyOb"]] = None
 
     # ┌─────────────────────────────────────────────────────────────────────────────────
     # │ STORE SETTINGS
     # └─────────────────────────────────────────────────────────────────────────────────
 
     # Initialize keys to None
-    keys = None
+    keys: list[str] = None
 
     # ┌─────────────────────────────────────────────────────────────────────────────────
     # │ AESTHETIC SETTINGS
     # └─────────────────────────────────────────────────────────────────────────────────
 
     # Initialize string field
-    string = None
+    string: str = None
 
     # Initialize labels to None
-    label_singular = label_plural = None
+    label_singular: str = None
+    label_plural: str = None
+
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ CIRCULAR IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from pyob.main.classes.pyob import PyOb  # noqa
+
+PyOb.update_forward_refs()

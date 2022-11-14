ad06c84806964415be1e15d2d7281dafc2438cd5
Sean Michael O'Leary <seamicole@gmail.com>
2022-11-15 01:18:39 +0800

    Add PyObMetaClass constraint tests

--- tests/meta/classes/test_pyob_meta_class.py
+++ tests/meta/classes/test_pyob_meta_class.py
@@ -26,5 +26,39 @@ class TestPyObMetaClass:
         # Assert that PyObMeta Parents is an empty list
         assert type(PyObMeta.Parents) is list and len(PyObMeta.Parents) == 0
 
-        # Assert that PyObMeta Parents is an empty list
+        # Assert that PyObMeta Children is an empty list
         assert type(PyObMeta.Children) is list and len(PyObMeta.Children) == 0
+
+    # ┌─────────────────────────────────────────────────────────────────────────────────
+    # │ TEST INIT FREEZES CONSTRAINTS
+    # └─────────────────────────────────────────────────────────────────────────────────
+
+    def test_init_freezes_constraints(self) -> None:
+        """Tests that constraints passed into the PyObMetaClass instance are frozen"""
+
+        # Create a PyObMetaClass instance without constraints
+        UnconstrainedPyObMeta = PyObMetaClass()
+
+        # Assert that PyObMeta uniques is an empty frozenset
+        assert UnconstrainedPyObMeta.uniques == frozenset([])
+
+        # Assert that PyObMeta indexes is an empty frozenset
+        assert UnconstrainedPyObMeta.indexes == frozenset([])
+
+        # Create another PyObMetaClass instance with constraints
+        ConstrainedPyObMeta = PyObMetaClass(
+            uniques=("a", "b", ("c", "d", "c"), "a"),
+            indexes=("e", "f", ("g", "h", "g"), "e"),
+        )
+
+        # Assert that uniques is now a frozenset
+        assert type(ConstrainedPyObMeta.uniques) is frozenset
+
+        # Assert that unique fields are correct
+        assert ConstrainedPyObMeta.uniques == {"a", "b", {"c", "d"}}
+
+        # Assert that indexes is now a frozenset
+        assert type(ConstrainedPyObMeta.indexes) is frozenset
+
+        # Assert that indexed fields are correct
+        assert ConstrainedPyObMeta.indexes == {"e", "f", {"g", "h"}}

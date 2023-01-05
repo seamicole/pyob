92085a86c3e57ab07e02835535e4fe6a0aa43630
Sean O'Leary <seamicole@gmail.com>
2023-01-05 20:17:34 +0700

    Add PyObSet test for removing zero counts

--- tests/set/classes/test_pyob_set.py
+++ tests/set/classes/test_pyob_set.py
@@ -43,11 +43,11 @@ class TestPyObSet:
         assert isinstance(dummy_set, PyObs)
 
     # ┌─────────────────────────────────────────────────────────────────────────────────
-    # │ TEST PYOB SET FREZES COUNTS
+    # │ TEST INIT FREZES COUNTS
     # └─────────────────────────────────────────────────────────────────────────────────
 
-    def test_pyob_set_freezes_counts(self) -> None:
-        """Tests that PyObSet instances cast counts attribute to a FrozenDict"""
+    def test_init_freezes_counts(self) -> None:
+        """Tests that init method casts counts attribute to a FrozenDict"""
 
         # Define a dummy PyObClass
         class DummyClass(PyOb):
@@ -64,6 +64,31 @@ class TestPyObSet:
 
         # Assert that dummy set counts is a FrozenDict
         assert type(dummy_set._counts) is FrozenDict
+
+    # ┌─────────────────────────────────────────────────────────────────────────────────
+    # │ TEST INIT REMOVES ZERO COUNTS
+    # └─────────────────────────────────────────────────────────────────────────────────
+
+    def test_init_removes_zero_counts(self) -> None:
+        """Tests that init method removes PyObs with a count of zero"""
+
+        # Define a dummy PyObClass
+        class DummyClass(PyOb):
+            """A dummy PyObClass"""
+
+        # Initialize dummy instances
+        d1, d2, d3, d4 = (DummyClass(), DummyClass(), DummyClass(), DummyClass())
+
+        # Define counts
+        _counts = {d1: 1, d2: 0, d3: 3, d4: -6}
+
+        # Initialize dummy set
+        dummy_set = PyObSet[DummyClass](_counts=_counts)
+
+        # Assert that PyObs with a count of zero or less have been removed
+        assert dummy_set._counts == {
+            pyob: count for pyob, count in _counts.items() if count > 0
+        }
 
     # ┌─────────────────────────────────────────────────────────────────────────────────
     # │ TEST LENGTH METHODS RETURN SUM OF COUNTS
